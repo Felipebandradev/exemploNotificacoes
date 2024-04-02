1f9d07429defdd0b3e9041ecbc11d39e28aac253
Felipe Barbosa Brito de Andrade <felipebandradedev@gmail.com>
2024-04-02 10:40:32 -0300

    feat: estrutura basica do app

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StatusBar, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const enviarMenssagem = () => {};
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar />
+      <View style={styles.container}>
+        <Text>Exemplo de notificação local</Text>
+        <Button title="Disparar notificação" onPress={enviarMenssagem} />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
